927e9432d87841bcb06f6e54414f218fafa0297c
Albert Chang <a89529294@hotmail.com>
2022-02-25 17:09:42 +0800

    .

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
 
   return (
     <div className="wrapper">
-      <div className="ring">
+      <div className={duration === 0 ? "ring ending" : "ring"}>
         <svg width="518" height="518" viewBox="0 0 518 518">
           <circle strokeWidth="9px" x="0" y="y" cx="259" cy="259" r="254" />
         </svg>
